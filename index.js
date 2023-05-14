fcfbb4aec0592c9bfacd7c677b5233a884aa9ea2
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2023-05-14 10:47:42 +0530

    sd

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({
-  dest: 'uploads/',
+  dest: "uploads/",
   fileFilter: (req, file, cb) => {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Only image files are allowed!'));
+      return cb(new Error("Only image files are allowed!"));
     }
     cb(null, true);
   },
   // limits: { fileSize: 1024 * 1024 }
 });
-const MongoClient = require('mongodb').MongoClient;
-const url =
-  'mongodb+srv://admin:Dikush@cluster0.nnh08zd.mongodb.net/expressTest?retryWrites=true&w=majority';
-const path = require('path');
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGOURI;
+console.log(process.env.MONGOURI)
+const path = require("path");
 
-const collectionName = 'myCollection';
+const collectionName = "myCollection";
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const username = req.query.username;
   const emailID = req.query.emailID;
   const requestID = req.query.requestID;
   // console.log(username);
-  res.render('form', { username,emailID,requestID });
+  res.render("form", { username, emailID, requestID });
 });
 
-app.post('/', upload.single('image'), async (req, res) => {
+app.post("/", upload.single("image"), async (req, res) => {
   const imageUrl = req.file.path;
-  const username=req.body.username
-  const emailID=req.body.emailID
-  const requestID=req.body.requestID
+  const username = req.body.username;
+  const emailID = req.body.emailID;
+  const requestID = req.body.requestID;
   // console.log(imageUrl);
   let client = null;
   try {
     const obj = {
       username: username,
-      emailID:emailID,
-      requestID:requestID,
-      imageUrl: imageUrl
+      emailID: emailID,
+      requestID: requestID,
+      imageUrl: imageUrl,
     };
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     await client.connect();
     const collection = client.db().collection(collectionName);
     const result = await collection.insertOne(obj);
     res.json(result);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal server error');
+    res.status(500).send("Internal server error");
   } finally {
     if (client) {
       await client.close();
@@ -57,25 +60,24 @@ app.post('/', upload.single('image'), async (req, res) => {
   }
 });
 
-const fs = require('fs');
+const fs = require("fs");
 
-app.get('/image/:filename', (req, res) => {
+app.get("/image/:filename", (req, res) => {
   const filename = req.params.filename;
-  const filepath = path.join(__dirname, 'uploads', filename);
+  const filepath = path.join(__dirname, "uploads", filename);
   fs.readFile(filepath, (err, data) => {
     if (err) {
-      res.status(404).send('File not found');
+      res.status(404).send("File not found");
     } else {
-      res.contentType('image/jpeg');
+      res.contentType("image/jpeg");
       res.send(data);
     }
   });
 });
 
-
 app.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
-    res.status(400).send('File upload error');
+    res.status(400).send("File upload error");
   } else if (err) {
     res.status(400).send(err.message);
   } else {
@@ -84,7 +86,7 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8000, () => {
-  console.log('Server listening on port 8000');
+  console.log("Server listening on port 8000");
 });
 
 module.exports = app;
